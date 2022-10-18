f48ca5bf7478e8b7dbe29b82e9d90cff7374cb31
Anastasiya Z <nastya_tim2201@mail.ru>
2022-10-18 15:45:10 +0300

    решили первую задачку

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Дано число, обозначающее день недели. Выяснить, является ли номер дня недели выходным.
+
+Console.Clear();
+Console.WriteLine("Введите номер дня недели (от 1 до 7): ");
+int number = int.Parse(Console.ReadLine());
+
+if(number < 1 || number > 8)
+    Console.WriteLine("Вы ввели неверное число");
+else
+    if(number > 0 && number < 6)
+        Console.WriteLine("Это будний день");
+    else
+        Console.WriteLine("Это выходной");
