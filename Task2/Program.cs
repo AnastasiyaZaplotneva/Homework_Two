af24f099365e0595fb1322788c983871a378fa9f
Anastasiya Z <nastya_tim2201@mail.ru>
2022-10-18 16:02:41 +0300

    решили вторую задачку

---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// По двум заданным числам проверять, является ли одно квадратом другого.
+
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int numA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numB = int.Parse(Console.ReadLine());
+if(numA*numA == numB)
+    Console.WriteLine($"Число {numB} является квадратом числа {numA}");
+else
+    if(numB*numB == numA)
+        Console.WriteLine($"Число {numA} является квадратом числа {numB}");
+    else
+        Console.WriteLine("Ни одно из введенных чисел не является квадратом другого");
+
+// Интереса ради проверила еще один вариант, принцип тот же, но с использованием логического оператора. В общем, оба варианта рабочие.
+//
+// if(numA*numA == numB || numB*numB == numA)
+//     if(numA > numB)
+//         Console.WriteLine($"Число {numA} является квадратом числа {numB}");
+//     else
+//         Console.WriteLine($"Число {numB} является квадратом числа {numA}"); 
+// else
+//     Console.WriteLine("Ни одно из введенных чисел не является квадратом другого");
